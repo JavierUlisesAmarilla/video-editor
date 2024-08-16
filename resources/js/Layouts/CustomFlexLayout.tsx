de669ba8233370e6cc850218e59760af4eae5c6d
JavierUlisesAmarilla <amarillajavierulises@gmail.com>
2024-08-16 18:08:06 -0300

    flex layout mvp

---
+++ resources/js/Layouts/CustomFlexLayout.tsx
@@ -0,0 +1,23 @@
+import * as FlexLayout from "flexlayout-react"
+import "flexlayout-react/style/light.css"
+import { ReactNode } from "react"
+
+export const CustomFlexLayout = ({
+    json,
+    components,
+}: {
+    json: FlexLayout.IJsonModel;
+    components: Record<string, ReactNode>;
+}) => {
+    const factory = (node: FlexLayout.TabNode) => {
+        const component = node.getComponent() as string
+        return components[component]
+    }
+
+    return (
+        <FlexLayout.Layout
+            model={FlexLayout.Model.fromJson(json)}
+            factory={factory}
+        />
+    )
+}
