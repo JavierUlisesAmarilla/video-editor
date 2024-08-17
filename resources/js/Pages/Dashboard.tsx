9ba197b2a9c6cea7a5ddf5554b152b6c77aced82
JavierUlisesAmarilla <amarillajavierulises@gmail.com>
2024-08-16 21:19:28 -0300

    enhance layout

--- resources/js/Pages/Dashboard.tsx
+++ resources/js/Pages/Dashboard.tsx
@@ -5,77 +5,75 @@ import { CustomFlexLayout } from "@/Layouts/CustomFlexLayout"
 
 export default function Dashboard({ auth }: PageProps) {
     return (
-        <CustomFlexLayout
-            components={{
-                slide: <div>Slide</div>,
-                scene: (
-                    <AuthenticatedLayout user={auth.user}>
-                        <Head title="Dashboard"/>
-                        <div>Scene</div>
-                    </AuthenticatedLayout>
-                ),
-                asset: <div>Asset</div>,
-                timeline: <div>Timeline</div>,
-            }}
-            json={{
-                global: {
-                    tabEnableClose: false,
-                    tabEnableRename: false,
-                    tabSetEnableMaximize: false,
-                },
-                borders: [
-                    {
-                        type: "border",
-                        location: "bottom",
-                        size: 250,
-                        children: [
-                            {
-                                type: "tab",
-                                name: "Timeline",
-                                component: "timeline",
-                            },
-                        ],
+        <AuthenticatedLayout user={auth.user}>
+            <Head title="Dashboard"/>
+            <CustomFlexLayout
+                components={{
+                    slide: <div>Slide</div>,
+                    scene: <div>Scene</div>,
+                    asset: <div>Asset</div>,
+                    timeline: <div>Timeline</div>,
+                }}
+                json={{
+                    global: {
+                        tabEnableClose: false,
+                        tabEnableRename: false,
+                        tabSetEnableMaximize: false,
                     },
-                ],
-                layout: {
-                    type: "row",
-                    children: [
+                    borders: [
                         {
-                            type: "tabset",
-                            weight: 1,
+                            type: "border",
+                            location: "bottom",
+                            size: 250,
                             children: [
                                 {
                                     type: "tab",
-                                    name: "Slide",
-                                    component: "slide",
-                                },
-                            ],
-                        },
-                        {
-                            type: "tabset",
-                            weight: 7,
-                            children: [
-                                {
-                                    type: "tab",
-                                    name: "Scene",
-                                    component: "scene",
-                                },
-                            ],
-                        },
-                        {
-                            type: "tabset",
-                            weight: 2,
-                            children: [
-                                {
-                                    type: "tab",
-                                    name: "Asset",
-                                    component: "asset",
+                                    name: "Timeline",
+                                    component: "timeline",
                                 },
                             ],
                         },
                     ],
-                },
-            }}
-        />
+                    layout: {
+                        type: "row",
+                        children: [
+                            {
+                                type: "tabset",
+                                weight: 1,
+                                children: [
+                                    {
+                                        type: "tab",
+                                        name: "Slide",
+                                        component: "slide",
+                                    },
+                                ],
+                            },
+                            {
+                                type: "tabset",
+                                weight: 7,
+                                children: [
+                                    {
+                                        type: "tab",
+                                        name: "Scene",
+                                        component: "scene",
+                                    },
+                                ],
+                            },
+                            {
+                                type: "tabset",
+                                weight: 2,
+                                children: [
+                                    {
+                                        type: "tab",
+                                        name: "Asset",
+                                        component: "asset",
+                                    },
+                                ],
+                            },
+                        ],
+                    },
+                }}
+            />
+        </AuthenticatedLayout>
     )
 }
