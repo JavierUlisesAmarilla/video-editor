0b6bdca56cd3c5522212b043ed122b9462640ff6
JavierUlisesAmarilla <amarillajavierulises@gmail.com>
2024-08-20 16:41:57 -0300

    text mvp

--- resources/js/Components/Editor/Asset/Text.tsx
+++ resources/js/Components/Editor/Asset/Text.tsx
@@ -3,21 +3,29 @@ import TextInput from "@/Components/TextInput"
 import {useState} from "react"
 import {HexColorPicker} from "react-colorful"
 import Switch from "react-custom-checkbox/switch"
+import {
+  BsAlignBottom,
+  BsAlignCenter,
+  BsAlignEnd,
+  BsAlignMiddle,
+  BsAlignStart,
+  BsAlignTop,
+  BsTextCenter,
+  BsTextLeft,
+  BsTextParagraph,
+  BsTextRight,
+} from "react-icons/bs"
 import Select from "react-select"
-
-const fountainOptions = [
-  { value: "arial", label: "Arial" },
-  { value: "verdana", label: "Verdana" },
-]
 
 export const Text = () => {
   const [isHexColorPickerVisible, setIsHexColorPickerVisible] = useState(false)
   const [textColor, setTextColor] = useState("#000000")
   const [isTransparent, setIsTransparent] = useState(false)
-  const [selectedFountainOption, setSelectedFountainOption] = useState(null)
+  const [selectedFountainOption, setSelectedFountainOption] = useState()
+  const [selectedWeightOption, setSelectedWeightOption] = useState()
 
   return (
-    <div className="p-2 flex flex-col gap-2">
+    <div className="p-4 flex flex-col gap-4">
       <div>
         <InputLabel htmlFor="text" value="Text"/>
         <TextInput
@@ -25,10 +33,9 @@ export const Text = () => {
           id="text"
           // value={data.name}
           // onChange={(e) => setData('name', e.target.value)}
-          isFocused
         />
       </div>
-      <div className="flex gap-1">
+      <div className="flex gap-4">
         <div
           className="w-12 h-12 rounded-full cursor-pointer"
           style={{ backgroundColor: textColor }}
@@ -38,7 +45,7 @@ export const Text = () => {
           <HexColorPicker color={textColor} onChange={setTextColor}/>
         )}
       </div>
-      <div className="flex gap-1 items-center">
+      <div className="flex gap-4 items-center">
         <div>Transparent</div>
         <Switch
           checked={isTransparent}
@@ -54,9 +61,93 @@ export const Text = () => {
           value={selectedFountainOption}
           // @ts-expect-error -- TODO
           onChange={setSelectedFountainOption}
-          options={fountainOptions}
+          options={[
+            { value: "arial", label: "Arial" },
+            { value: "verdana", label: "Verdana" },
+          ]}
           isSearchable={false}
         />
+      </div>
+      <div>
+        <InputLabel htmlFor="weight" value="Weight"/>
+        <Select
+          className="mt-1 block w-full"
+          id="weight"
+          value={selectedWeightOption}
+          // @ts-expect-error -- TODO
+          onChange={setSelectedWeightOption}
+          options={[
+            { value: "regular", label: "Regular" },
+            { value: "italic", label: "Italic" },
+            { value: "semiBold", label: "Semi-Bold" },
+            { value: "bold", label: "Bold" },
+          ]}
+          isSearchable={false}
+        />
+      </div>
+      <div className="flex gap-4">
+        <div className="w-full">
+          <InputLabel htmlFor="size" value="Size"/>
+          <TextInput className="mt-1 block w-full" id="size"/>
+        </div>
+        <div className="w-full">
+          <InputLabel htmlFor="lineSpacing" value="Line Spacing"/>
+          <TextInput className="mt-1 block w-full" id="lineSpacing"/>
+        </div>
+      </div>
+      <div className="flex gap-4">
+        <div className="w-full">
+          <InputLabel htmlFor="spacing" value="Spacing"/>
+          <TextInput className="mt-1 block w-full" id="spacing"/>
+        </div>
+        <div className="w-full">
+          <InputLabel htmlFor="thickness" value="Thickness"/>
+          <TextInput className="mt-1 block w-full" id="thickness"/>
+        </div>
+      </div>
+      <div className="flex flex-col gap-1">
+        <div>Align</div>
+        <div className="flex gap-4">
+          <BsTextLeft className="border text-3xl rounded cursor-pointer"/>
+          <BsTextCenter className="border text-3xl rounded cursor-pointer"/>
+          <BsTextRight className="border text-3xl rounded cursor-pointer"/>
+          <BsTextParagraph className="border text-3xl rounded cursor-pointer"/>
+        </div>
+      </div>
+      <div className="flex flex-col gap-1">
+        <div>Position</div>
+        <div className="flex justify-between">
+          <div className="flex flex-col gap-1">
+            <div className="flex items-center gap-1">
+              <BsAlignTop className="border text-3xl rounded cursor-pointer"/>
+              Top
+            </div>
+            <div className="flex items-center gap-1">
+              <BsAlignBottom className="border text-3xl rounded cursor-pointer"/>
+              Bottom
+            </div>
+          </div>
+          <div className="flex flex-col gap-1">
+            <div className="flex items-center gap-1">
+              <BsAlignMiddle className="border text-3xl rounded cursor-pointer"/>
+              Middle
+            </div>
+            <div className="flex items-center gap-1">
+              <BsAlignCenter className="border text-3xl rounded cursor-pointer"/>
+              Center
+            </div>
+          </div>
+          <div className="flex flex-col gap-1">
+            <div className="flex items-center gap-1">
+              <BsAlignStart className="border text-3xl rounded cursor-pointer"/>
+              Left
+            </div>
+            <div className="flex items-center gap-1">
+              <BsAlignEnd className="border text-3xl rounded cursor-pointer"/>
+              Right
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
