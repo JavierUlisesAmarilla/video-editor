a38947a0d41c40b3c0b12ef1d5321b1a2e540140
JavierUlisesAmarilla <amarillajavierulises@gmail.com>
2024-08-21 13:11:23 -0300

    background

--- resources/js/Components/Editor/R3fScene/R3fWorld.tsx
+++ resources/js/Components/Editor/R3fScene/R3fWorld.tsx
@@ -1,6 +1,15 @@
 /* eslint-disable react/no-unknown-property */
 import {AXES_LENGTH, SHOW_AXES_HELPER} from "@/utils/constants"
+import {useThree} from "@react-three/fiber"
+import {useEffect} from "react"
+import {Color} from "three"
 
 export const R3fWorld = () => {
+  const { scene } = useThree()
+
+  useEffect(() => {
+    scene.background = new Color("yellow")
+  }, [])
+
   return <>{SHOW_AXES_HELPER && <axesHelper args={[AXES_LENGTH]}/>}</>
 }
