d205ba24e8f9333a4424eebfd4d7a6b8979593b8
JavierUlisesAmarilla <amarillajavierulises@gmail.com>
2024-08-22 00:40:10 -0300

    objects table

--- app/Models/Object.php
+++ app/Models/Object.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Page extends Model
+class Object extends Model
 {
-    protected $table = "pages";
+    protected $table = "objects";
 }
