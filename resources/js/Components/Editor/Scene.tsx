5317236e88ae31ca2e16ec2318bd418c9af97972
JavierUlisesAmarilla <amarillajavierulises@gmail.com>
2024-08-22 08:14:25 -0300

    disable undo, redo button

--- resources/js/Components/Editor/Scene.tsx
+++ resources/js/Components/Editor/Scene.tsx
@@ -2,7 +2,7 @@ import {useZustand} from "@/store/useZustand"
 import {Page} from "@/types"
 import axios from "axios"
 import classNames from "classnames"
-import {BiRedo, BiUndo} from "react-icons/bi"
+// import {BiRedo, BiUndo} from "react-icons/bi"
 import {BsPlusSquare, BsTrash} from "react-icons/bs"
 import {toast} from "react-toast"
 import {R3fScene} from "./R3fScene/R3fScene"
@@ -15,16 +15,30 @@ export const Scene = () => {
     <div className="w-full h-full text-sm relative">
       <div className="absolute w-full flex justify-between items-center p-2 z-10 bg-white">
         <div className="flex gap-2 items-center">
-          <div className="flex gap-1 items-center p-1 border rounded cursor-pointer border-gray-500">
+          {/* <div className="flex gap-1 items-center p-1 border rounded cursor-pointer border-gray-500">
             <BiUndo className="text-base"/>
             <div>Undo</div>
           </div>
           <div className="flex gap-1 items-center p-1 border rounded cursor-pointer border-gray-500">
             <BiRedo className="text-base"/>
             <div>Redo</div>
-          </div>
+          </div> */}
         </div>
         <div className="flex gap-2 items-center">
+          <div
+            className="flex gap-1 items-center p-1 border rounded cursor-pointer border-gray-500"
+            onClick={async () => {
+              const newPage: Page = {}
+              const res = await axios.post("/savePage", newPage)
+              toast("New page created.")
+              newPage.id = res.data.id
+              addPage(newPage)
+              setSelPageId(newPage.id!)
+            }}
+          >
+            <BsPlusSquare className="text-base"/>
+            <div>New Page</div>
+          </div>
           <div
             className={classNames(
               "flex gap-1 items-center p-1 border rounded cursor-pointer border-gray-500",
@@ -46,20 +60,6 @@ export const Scene = () => {
           >
             <BsTrash className="text-base"/>
             <div>Remove Page</div>
-          </div>
-          <div
-            className="flex gap-1 items-center p-1 border rounded cursor-pointer border-gray-500"
-            onClick={async () => {
-              const newPage: Page = {}
-              const res = await axios.post("/savePage", newPage)
-              toast("New page created.")
-              newPage.id = res.data.id
-              addPage(newPage)
-              setSelPageId(newPage.id!)
-            }}
-          >
-            <BsPlusSquare className="text-base"/>
-            <div>New Page</div>
           </div>
         </div>
       </div>
